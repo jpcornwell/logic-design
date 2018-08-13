062284a9610698fb42ebfc2889fbaf0fa0b0a483
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-08-13 00:18:41 -0500

    Add some todos for combinational logic

--- 01-combinational-logic.py
+++ 01-combinational-logic.py
@@ -1,2 +1,8 @@
 from rattle import *
 
+# TODO: Explain the use of the terms product and sum when dealing with logic design (product = AND, sum = OR)
+# TODO: Touch upon universality (for example any logic function can be made using nothing but ANDs and ORs)
+# TODO: Write Python code that given a function (that represents a truth table), will automatically generate a corresponding circuit using product of sums
+# TODO: Explain multiplexers
+# TODO: Go over Boole's expansion theorem (also known as Shannon expansion)
+
