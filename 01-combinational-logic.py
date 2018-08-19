b936a9b750ade59f3472464f23cc4b2cec73d743
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-08-18 21:46:40 -0500

    Add some more notes

--- 01-combinational-logic.py
+++ 01-combinational-logic.py
@@ -5,4 +5,14 @@ from rattle import *
 # TODO: Write Python code that given a function (that represents a truth table), will automatically generate a corresponding circuit using product of sums
 # TODO: Explain multiplexers
 # TODO: Go over Boole's expansion theorem (also known as Shannon expansion)
+# TODO: Mention that XOR can be thought of as a conditional inverter
+#       x ^ y = when(x, ~y, y)
+# TODO: Define circuit for 8 bit comparison function
+# TODO: Define circuit for 8 bit constant comparer
+# TODO: Define logic for Ripple-carry adder
+# TODO: x ^ y = (x + y) mod 2
+# TODO: Define circuit for 4 bit Ripple-carry adder
+# TODO: Go over 2's complement
+# TODO: Define circuit for subtraction
+# TODO: Define simple ALU circuit that adds or subtracts based on flag
 
